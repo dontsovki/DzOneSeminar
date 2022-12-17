f7d8153e0aee2dd5ee1e625aa2361cb2d6d63573
Artem.don <dontsovki@gmail.com>
2022-12-17 12:34:32 +0300

    создали две задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("введите первое число ");
+int ferstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите второе число ");
+int Secondtnumber = Convert.ToInt32(Console.ReadLine());
+
+if (ferstnumber == Secondtnumber*Secondtnumber)
+    Console.Write($"Число {ferstnumber} является квадратом {Secondtnumber}");
+else 
+    Console.Write($"Число {ferstnumber} не является квадратом {Secondtnumber}");
+
+
+
+
+//Console.Write("введите число ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write($"квадрат числа  {number}  будет равен {number * number}");
